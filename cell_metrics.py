b30b822b89ebe2d2bc3ed52ef0bf92547ea1b957
hallvaaw <hallvard_wahler@hotmail.com>
2023-01-25 23:33:34 +0100

    import files and rename variables

--- cell_metrics.py
+++ cell_metrics.py
@@ -1,23 +1,25 @@
-# @String input_analysis
-# @String image_dir
-# @String cell_parameters
-# @String outlines
-# @Integer height
-# @Integer width
-# @String filenames
-# @String outputfiles
-# @String outlinefiles
-
-"""
-Jython script to perform cell metric quantification in Fiji
-"""
+""" Jython script to perform cell metric quantification in Fiji """
 
 from ij import IJ, ImagePlus
 from ij.plugin import ImageCalculator
 
+# Open file with parameters for image analysis
+with open("anda_parameters.txt", 'r') as anda_parameters:
+    analysis_read = anda_parameters.readlines()
+
+CELL_TYPE = str(anda_parameters[1])
+INPUT_ANALYSIS = str(anda_parameters[0]) # List
+IMAGE_DIR = str(anda_parameters[1])
+OUTLINES = str(anda_parameters[2])
+
+
+# Open file with image file names
+with open("file_names.txt", 'r') as file_names:
+    input_files = file_names.read().splitlines()
+
 # Open file with parameters for analyzing cell type
-with open("{}_parameters.txt".format(cell_parameters), 'r') as cell:
-    cell_read = cell.readlines()
+with open("{}_parameters.txt".format(CELL_TYPE), 'r') as cell_type:
+    cell_read = cell_type.readlines()
 
 cell_line_parameters = [str(i.rstrip()) for i in cell_read] # Cell parameters list
 PATH = str(cell_line_parameters[0])
@@ -44,11 +46,11 @@ if NEURITE_THRESHOLD == "NO THRESHOLD":
 IJ.run("Set Measurements...", "area fit shape display add redirect=None decimal=3")
 image_calc = ImageCalculator() # ImageJ plugin
 
-def particle_analysis(analysis):
+def particle_analysis(analysis, file_):
     """Quantify cell body count, neurite lengths and neurite attachment points"""
 
     if analysis == "cells": # Run cell body count analysis
-        IJ.open("{}/{}".format(str(image_dir), str(filenames)))
+        IJ.open(file_)
         imp = IJ.getImage()
         IJ.run("8-bit")
         IJ.run("Auto Threshold", "method={}".format(CELL_THRESHOLD))
@@ -58,11 +60,13 @@ def particle_analysis(analysis):
         else:
             IJ.run("Analyze Particles...", "size={}-{} pixel circularity={}-{} show=Nothing display summarize".format(min_cell_size, max_cell_size, min_cell_circularity, max_cell_circularity))
         IJ.saveAs("Results", "{}".format(str(outputfiles)))
-        IJ.run("Clear Results")
-        if outlines == "show_outlines":
+        # IJ.run("Clear Results")
+        if OUTLINES == "show_OUTLINES":
             IJ.run("Analyze Particles...", "size={}-{} pixel circularity={}-{} show=Nothing display summarize".format(min_cell_size, max_cell_size, min_cell_circularity, max_cell_circularity))
             overlay_ = ImagePlus.getOverlay(imp)
-            IJ.newImage("blank_c", "RGB white", width, height, 1)
+            image_width = imp.getWidth()
+            image_height = imp.getHeight()
+            IJ.newImage("blank_c", "RGB white", image_width, image_height, 1)
             imp2 = IJ.getImage().setOverlay(overlay_)
             imp2 = IJ.getImage()
             imp3 = imp2.flatten()
@@ -70,7 +74,7 @@ def particle_analysis(analysis):
             IJ.saveAs(imp3, "Tiff", "{}".format(str(outlinefiles)))
 
     elif analysis == "neurites": # Run neurite length analysis
-        IJ.open("{}/{}".format(str(image_dir), str(filenames)))
+        IJ.open(file_)
         imp = IJ.getImage()
         IJ.run("8-bit")
         IJ.run("Auto Threshold", "method={}".format(NEURITE_THRESHOLD))
@@ -81,11 +85,13 @@ def particle_analysis(analysis):
             IJ.run("Analyze Particles...", "size={}-{} pixel circularity={}-{} show=Nothing display summarize".format(min_cell_size, max_cell_size, min_cell_circularity, max_cell_circularity))
         IJ.run("Analyze Particles...", "size={}-{} pixel circularity={}-{} show=Nothing display summarize".format(min_neurite_size, max_neurite_size, min_neurite_circularity, max_neurite_circularity))
         IJ.saveAs("Results", "{}".format(str(outputfiles)))
-        IJ.run("Clear Results")
-        if outlines == "show_outlines":
+        # IJ.run("Clear Results")
+        if OUTLINES == "show_OUTLINES":
             IJ.run("Analyze Particles...", "size={}-{} pixel circularity={}-{} show=Nothing display summarize".format(min_cell_size, max_cell_size, min_cell_circularity, max_cell_circularity))
             overlay_ = ImagePlus.getOverlay(imp)
-            IJ.newImage("blank_n", "RGB white", width, height, 1)
+            image_width = imp.getWidth()
+            image_height = imp.getHeight()
+            IJ.newImage("blank_n", "RGB white", image_width, image_height, 1)
             imp2 = IJ.getImage().setOverlay(overlay_)
             imp2 = IJ.getImage()
             imp3 = imp2.flatten()
@@ -94,7 +100,7 @@ def particle_analysis(analysis):
 
 
     elif analysis == "attachment": # Run neurite attachment point analysis
-        img = IJ.open("{}/{}".format(str(image_dir), str(filenames)))
+        IJ.open(file_)
         imp_c = IJ.getImage() # Cell bodies
         imp_n = IJ.getImage() # Neurites
         IJ.run(imp_c, "8-bit", "")
@@ -119,7 +125,9 @@ def particle_analysis(analysis):
             overlay_n = ImagePlus.getOverlay(imp_n) # Get neurite overlay
 
         # Blank image for pasting the overlay onto
-        IJ.newImage("{}_blank_c".format(filenames), "RGB white", width, height, 1)
+        image_width = imp.getWidth()
+        image_height = imp.getHeight()
+        IJ.newImage("{}_blank_c".format(file_), "RGB white", image_width, image_height, 1)
         imp_c2 = IJ.getImage().setOverlay(overlay_c)
         imp_c2 = IJ.getImage()
         imp_c3 = imp_c2.flatten()
@@ -128,7 +136,9 @@ def particle_analysis(analysis):
         IJ.saveAs(imp_c3, "Tiff", "{}/imp_c3.tif".format(PATH))
 
         # Blank image for pasting the overlay onto
-        IJ.newImage("{}_blank_n".format(filenames), "RGB white", width, height, 1)
+        image_width = imp.getWidth()
+        image_height = imp.getHeight()
+        IJ.newImage("{}_blank_n".format(file_), "RGB white", image_width, image_height, 1)
         imp_n2 = IJ.getImage().setOverlay(overlay_n)
         imp_n2 = IJ.getImage()
         imp_n3 = imp_n2.flatten()
@@ -138,22 +148,25 @@ def particle_analysis(analysis):
         IJ.run(imp_n3, "Find Edges", "") # Sobel edge detector
 
         # Get the overlap between cell bodies and neurites - the attachment points
-        imp_res = ic.run("Multiply create", imp_n3, imp_c3)
-        IJ.saveAs(imp_res, "Tiff", "{}/{}_imp_res.tif".format(PATH, filenames))
+        imp_res = image_calculator.run("Multiply create", imp_n3, imp_c3)
+        IJ.saveAs(imp_res, "Tiff", "{}/{}_imp_res.tif".format(PATH, file_))
         IJ.run(imp_res, "Analyze Particles...", "size=0-100 pixel circularity=0.00-1.00 show=Nothing display summarize")
         IJ.saveAs("Results", "{}".format(str(outputfiles)))
         IJ.run("Clear Results")
-        if outlines == "show_outlines":
+        if OUTLINES == "show_OUTLINES":
             IJ.run("Analyze Particles...", "size={}-{} pixel circularity={}-{} show=Nothing display summarize".format(min_cell_size, max_cell_size, min_cell_circularity, max_cell_circularity))
             overlay_ = ImagePlus.getOverlay(imp_res)
-            IJ.newImage("blank_b", "RGB white", width, height, 1)
+            image_width = imp.getWidth()
+            image_height = imp.getHeight()
+            IJ.newImage("blank_b", "RGB white", image_width, image_height, 1)
             imp2 = IJ.getImage().setOverlay(overlay_)
             imp2 = IJ.getImage()
             imp3 = imp2.flatten()
             IJ.run(imp3, "8-bit", "")
             IJ.saveAs(imp3, "Tiff", "{}".format(str(outlinefiles)))
 
-particle_analysis(input_analysis)
+for file_name in input_files:
+    particle_analysis(INPUT_ANALYSIS, file_name)
 
 def file_read():
     """If ouputfile is not written, write an empty outputfile"""
