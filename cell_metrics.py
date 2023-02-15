3fa0d5a07c75050450400b7011a8bfed6602beb0
hallvaaw <hallvard_wahler@hotmail.com>
2023-02-15 10:58:39 +0100

    renamed parameters

--- cell_metrics.py
+++ cell_metrics.py
@@ -9,7 +9,7 @@ with open("anda_parameters.txt", 'r') as anda_parameters:
     analysis_read = anda_parameters.readlines()
 
 CELL_LINE = str(anda_parameters[1])
-INPUT_ANALYSIS = str(anda_parameters[0]) # List
+# INPUT_ANALYSIS = str(anda_parameters[0]) # List
 IMAGE_DIR = str(anda_parameters[1])
 OUTLINES = str(anda_parameters[2])
 
@@ -39,9 +39,9 @@ NEURITE_THRESHOLD = str(cell_line_parameters[10])
 WATERSHED_CHOICE = str(cell_line_parameters[11])
 
 
-if CELL_THRESHOLD == "NO THRESHOLD":
+if CELL_THRESHOLD == "none":
     CELL_THRESHOLD = "Default" # Default thresholding method in ImageJ
-if NEURITE_THRESHOLD == "NO THRESHOLD":
+if NEURITE_THRESHOLD == "none":
     NEURITE_THRESHOLD = "Default" # Default thresholding method in ImageJ
 
 IJ.run("Set Measurements...", "area fit shape display add redirect=None decimal=3")
@@ -52,8 +52,8 @@ image_calc = ImageCalculator() # ImageJ plugin
 def cell_analysis(file_):
     """Quantify cell body count"""
 
-    IJ.open(file_)
-    imp = IJ.getImage()
+    # IJ.open(file_)
+    # imp = IJ.getImage()
     IJ.run("8-bit")
     IJ.run("Auto Threshold", "method={}".format(CELL_THRESHOLD))
     if WATERSHED_CHOICE == "apply_watershed":
@@ -63,7 +63,7 @@ def cell_analysis(file_):
         IJ.run("Analyze Particles...", "size={}-{} pixel circularity={}-{} show=Nothing display summarize".format(min_cell_size, max_cell_size, min_cell_circularity, max_cell_circularity))
     IJ.saveAs("Results", "{}".format(str(outputfiles)))
     # IJ.run("Clear Results")
-    if OUTLINES == "show_outlines":
+    if OUTLINES == "save_outlines":
         IJ.run("Analyze Particles...", "size={}-{} pixel circularity={}-{} show=Nothing display summarize".format(min_cell_size, max_cell_size, min_cell_circularity, max_cell_circularity))
         overlay_ = ImagePlus.getOverlay(imp)
         image_width = imp.getWidth()
@@ -79,8 +79,8 @@ def cell_analysis(file_):
 def neurite_analysis(file_):
     """Quantify neurite lengths"""
 
-    IJ.open(file_)
-    imp = IJ.getImage()
+    # IJ.open(file_)
+    # imp = IJ.getImage()
     IJ.run("8-bit")
     IJ.run("Auto Threshold", "method={}".format(NEURITE_THRESHOLD))
     if WATERSHED_CHOICE == "apply_watershed":
@@ -91,7 +91,7 @@ def neurite_analysis(file_):
     IJ.run("Analyze Particles...", "size={}-{} pixel circularity={}-{} show=Nothing display summarize".format(min_neurite_size, max_neurite_size, min_neurite_circularity, max_neurite_circularity))
     IJ.saveAs("Results", "{}".format(str(outputfiles)))
     # IJ.run("Clear Results")
-    if OUTLINES == "show_outlines":
+    if OUTLINES == "save_outlines":
         IJ.run("Analyze Particles...", "size={}-{} pixel circularity={}-{} show=Nothing display summarize".format(min_cell_size, max_cell_size, min_cell_circularity, max_cell_circularity))
         overlay_ = ImagePlus.getOverlay(imp)
         image_width = imp.getWidth()
@@ -107,9 +107,12 @@ def neurite_analysis(file_):
 def attachment_analysis(file_):
     """Quantify neurite attachment points"""
 
-    IJ.open(file_)
-    imp_c = IJ.getImage() # Cell bodies
-    imp_n = IJ.getImage() # Neurites
+    # IJ.open(file_)
+    imp_c = imp # Cell bodies
+    imp_n = imp # Neurites
+
+    # imp_c = IJ.getImage() # Cell bodies
+    # imp_n = IJ.getImage() # Neurites
     IJ.run(imp_c, "8-bit", "")
     IJ.run(imp_n, "8-bit", "")
 
@@ -132,8 +135,8 @@ def attachment_analysis(file_):
         overlay_n = ImagePlus.getOverlay(imp_n) # Get neurite overlay
 
     # Blank image for pasting the overlay onto
-    image_width = imp.getWidth()
-    image_height = imp.getHeight()
+    image_width = imp_c.getWidth()
+    image_height = imp_c.getHeight()
     IJ.newImage("{}_blank_c".format(file_), "RGB white", image_width, image_height, 1)
     imp_c2 = IJ.getImage().setOverlay(overlay_c)
     imp_c2 = IJ.getImage()
@@ -143,8 +146,8 @@ def attachment_analysis(file_):
     IJ.saveAs(imp_c3, "Tiff", "{}/imp_c3.tif".format(PATH))
 
     # Blank image for pasting the overlay onto
-    image_width = imp.getWidth()
-    image_height = imp.getHeight()
+    image_width = imp_c.getWidth()
+    image_height = imp_c.getHeight()
     IJ.newImage("{}_blank_n".format(file_), "RGB white", image_width, image_height, 1)
     imp_n2 = IJ.getImage().setOverlay(overlay_n)
     imp_n2 = IJ.getImage()
@@ -155,16 +158,16 @@ def attachment_analysis(file_):
     IJ.run(imp_n3, "Find Edges", "") # Sobel edge detector
 
     # Get the overlap between cell bodies and neurites - the attachment points
-    imp_res = image_calculator.run("Multiply create", imp_n3, imp_c3)
+    imp_res = image_calc.run("Multiply create", imp_n3, imp_c3)
     IJ.saveAs(imp_res, "Tiff", "{}/{}_imp_res.tif".format(PATH, file_))
     IJ.run(imp_res, "Analyze Particles...", "size=0-100 pixel circularity=0.00-1.00 show=Nothing display summarize")
     IJ.saveAs("Results", "{}".format(str(outputfiles)))
     IJ.run("Clear Results")
-    if OUTLINES == "show_outlines":
+    if OUTLINES == "save_outlines":
         IJ.run("Analyze Particles...", "size={}-{} pixel circularity={}-{} show=Nothing display summarize".format(min_cell_size, max_cell_size, min_cell_circularity, max_cell_circularity))
         overlay_ = ImagePlus.getOverlay(imp_res)
-        image_width = imp.getWidth()
-        image_height = imp.getHeight()
+        image_width = imp_c.getWidth()
+        image_height = imp_c.getHeight()
         IJ.newImage("blank_b", "RGB white", image_width, image_height, 1)
         imp2 = IJ.getImage().setOverlay(overlay_)
         imp2 = IJ.getImage()
@@ -178,9 +181,15 @@ executor = Executors.newFixedThreadsPool(3)
 
 # Perform image analysis of 3 metrics at a time
 for file_name in input_files:
-    executor.submit(cell_analysis, file_name)
-    executor.submit(neurite_analysis, file_name)
-    executor.submit(attachment_analysis, file_name)
+    IJ.open(file_name)
+    imp = IJ.getImage()
+    executor.submit(cell_analysis, imp)
+    executor.submit(neurite_analysis, imp)
+    executor.submit(attachment_analysis, imp)
+
+    # executor.submit(cell_analysis, file_name)
+    # executor.submit(neurite_analysis, file_name)
+    # executor.submit(attachment_analysis, file_name)
 
 def file_read():
     """If ouputfile is not written, write an empty outputfile"""
